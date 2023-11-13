e4dcdc9f45a892efc43b6f8c784e39691c63c25b
JayDbb <justin670brown@gmail.com>
2023-11-12 20:06:03 -0500

    elp

--- index.js
+++ index.js
@@ -142,7 +142,7 @@ const Register = (e)=>{
     document.getElementById("container-1").style.animation = "disappear 1s linear 1 both";
    
     setTimeout(()=>{
-        location.href = "theMultipliers/game.html"
+        location.href = "/theMultipliers/game.html"
 
     }, 1000)
 
